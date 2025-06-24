80e16a9cf924a6fc27f30d9bc4534bd0dbccfe8e
Christopher E. Siu <cesiu@calpoly.edu>
2025-06-24 09:34:11 -0700

    Added examples of recursion.

--- 06.24-recursion/fibonacci.py
+++ 06.24-recursion/fibonacci.py
@@ -2,20 +2,38 @@ import sys
 
 
 def fibonacci(n):
-    current = previous = None
+    # NOTE: In general, computing a Fibonacci number requires knowing the two
+    #       previous Fibonacci numbers:
+    #
+    # current = previous = None
+    #
+    # for i in range(n + 1):
+    #     print("i: %r, current: %r, previous: %r" % (i, current, previous))
+    #     if i == 0:
+    #         current = 0
+    #     elif i == 1:
+    #         previous = current
+    #         current = 1
+    #     else:
+    #         temp = previous
+    #         previous = current
+    #         current = current + temp
+    #
+    # return current
 
-    for i in range(n + 1):
-        if i == 0:
-            current = 0
-        elif i == 1:
-            previous = current
-            current = 1
-        else:
-            temp = previous
-            previous = current
-            current = current + temp
-
-    return current
+    # NOTE: If we instead solve this problem recursively, because the problem
+    #       is itself recursively defined, the recursive definition of the
+    #       problem tells us the smallest possible problems as well as how to
+    #       find smaller versions of the same problem:
+    if n == 0:
+        print("n: %d, f(n): %d" % (0, 0))
+        return 0
+    elif n == 1:
+        print("n: %d, f(n): %d" % (1, 1))
+        return 1
+    else:
+        print("n: %d, f(n): %d + %d" % (n, fibonacci(n - 1), fibonacci(n - 2)))
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 print(fibonacci(5))
--- 06.24-recursion/max_element.py
+++ 06.24-recursion/max_element.py
@@ -1,9 +1,40 @@
 def max_element(lst):
-    temp = None
-    for element in lst:
-        if temp is None or element > temp:
-            temp = element
-    return temp
+    """
+    Find the largest element in a list.
+
+    :param lst: A list of comparable elements
+    :return: The largest element in that list, or None if empty
+    """
+    
+    # NOTE: Iteration is the repeated application of a procedure. Here, the
+    #       function "iterates over" the elements within a list:
+    #
+    # temp = None
+    # for element in lst:
+    #     if temp is None or element > temp:
+    #         temp = element
+    # return temp
+
+    # NOTE: A function should solve a problem -- it should take as argument(s)
+    #       an instance of the problem, and it should produce as return value
+    #       a solution to that problem. Here, the list of integers is the
+    #       problem, and the largest integer in that list is the solution. A
+    #       recursive function calls itself, thus, a recursive function must
+    #       within itself solve the *same problem*.
+
+    # NOTE: One or more base cases should solve the smallest versions of the
+    #       problem non-recursively. Here, the smallest possible lists are the
+    #       lists of zero or one elements:
+    if len(lst) == 0:
+        return None
+    elif len(lst) == 1:
+        return lst[0]
+
+    # NOTE: One or more recursive cases should address larger versions of the
+    #       problem by reducing them to smaller versions of the *same problem*.
+    #       Here, the problem is a list, thus, a smaller version of the same
+    #       problem is a shorter list containing some of the same elements:
+    return max(lst[0], max_element(lst[1:]))
 
 
 print(max_element([2, -1, 9, 8, 5, -3, 0, 8]))
